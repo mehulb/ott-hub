a171f0c1dcf3fcf82aba156accf7e93015e8e667
mehul.bhavani <mehul.bhavani@hp.com>
2021-01-17 16:06:23 +0530

    initial commit

---
+++ OTTHub/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  OTTHub
+//
+//  Created by Mehul Bhavani on 17/01/21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
